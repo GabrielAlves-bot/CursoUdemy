a9616aa8f9cd8402977cbab8f8f5f8379c6a4f63
Gabriel Alves <gabrielalves00df@gmail.com>
2022-05-09 01:11:04 -0300

    Criando Projeto

---
+++ src/main/java/br/com/erudio/ProjetoRestSpringBoot/ProjetoRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.ProjetoRestSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erudio/ProjetoRestSpringBoot/ProjetoRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.ProjetoRestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
